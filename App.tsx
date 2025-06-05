6f20500544406d6ba6edbf8063b5a9d7a32cb2f3
leomansur999 <leonardomansurcastillo@gmail.com>
2025-06-05 16:55:41 -0300

    atualizando App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/Navigation/AppNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+
+import LocalizacaoAtingida from './src/Screens/LocalizacaoAtingida';
+import PanoramaGeral from './src/Screens/PanoramaGeral';
+import Prejuizos from './src/Screens/Prejuizos';
+import Recomendacoes from './src/Screens/Recomendacoes';
+import TempoInterrupcao from './src/Screens/TempoInterrupcao';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator initialRouteName="PanoramaGeral">
+        <Stack.Screen name="Panorama Geral" component={PanoramaGeral} />
+        <Stack.Screen name="Localização Atingida" component={LocalizacaoAtingida} />
+        <Stack.Screen name="Tempo de Interrupção" component={TempoInterrupcao} />
+        <Stack.Screen name="Prejuízos Causados" component={Prejuizos} />
+        <Stack.Screen name="Recomendações" component={Recomendacoes} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
